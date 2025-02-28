39b1fb02c5dd5dc8998dbcf3a8c95445b2bd01fc
creeper-byte <rodneycalvin33@gmail.com>
2025-02-28 11:45:34 +0300

    Revert "merge"

--- BACKEND/SDPapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Department, Issue
-
-# Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(Department)
-admin.site.register(Issue)
