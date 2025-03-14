96331d7f1f984148ae93ce6e2068df357a599323
NicoleMariah186 <nicolemariah256@gmail.com>
2025-03-14 08:18:45 +0300

    Just removed a file

--- ATIS/settings.py
+++ ATIS/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Members',
-    'Atis_apis'
+    'Members'
+    
 ]
 
 MIDDLEWARE = [
--- Atis_apis/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MembersApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Atis_apis'
