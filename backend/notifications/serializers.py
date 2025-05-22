d87c7cae87fda002ced77f654740774baaf98bf8
RitaAkankunda <akankundarita04@gmail.com>
2025-05-22 17:06:51 +0300

    Revert "updates"
    This reverts commit bc6881910c9f6eb59dd42e65cd170223fa90fdcc.

--- backend/notifications/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Notification
-class NotificationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notification
-        fields = ['id', 'user', 'notification_type', 'priority', 'title', 'message', 'is_read', 'created_at', 'sent_at', 'content_type', 'object_id', 'related_object']
-        read_only_fields = ['user', 'created_at', 'sent_at', 'content_type', 'object_id', 'related_object'] 
